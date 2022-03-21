e78d5bf86938153465bad774a103cae7206c8c5f
Angelo Campana Neto <netocampana@gmail.com>
2022-03-21 12:45:11 -0300

    Project to study Spring Boot Funcionalities on a CRUD. Initial Commit.

---
+++ src/main/java/com/netocampana/springbootessentials/controller/AnimeController.java
@@ -0,0 +1,31 @@
+package com.netocampana.springbootessentials.controller;
+
+
+import com.netocampana.springbootessentials.entities.Anime;
+import com.netocampana.springbootessentials.util.DataUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Slf4j
+@RequiredArgsConstructor
+public class AnimeController {
+
+
+    private final DataUtil dataUtil;
+
+    @GetMapping
+    public List<Anime> listAll(){
+        log.info("Date formatted {}", dataUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return Arrays.asList(new Anime("DBZ"), new Anime("Yu yu hakusho"));
+    }
+
+}
